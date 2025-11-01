7c039e1f0557f885493bfd2861cc9aafd739e10c
Kim Tae Sung <kaoss12@naver.com>
2025-11-01 17:52:54 +0900

    클라이언트 1차수정

--- webproxy-lab/Echo/echo_client.c
+++ webproxy-lab/Echo/echo_client.c
@@ -32,14 +32,21 @@ int main (int argc, char ** argv)
 
         else
         {
-            printf("somthing really fucked");
+            fprintf("somthing really fucked", strerror);
             exit(-1);
         }
     }
 
+    
     Rio_readinitb(&rio, clientfd); // 리오 버퍼 초기화
-    Rio_writen(clientfd, buf, MAXLINE); 
-    fputs(buf,stdout);
+
+    while(Fgets(buf, MAXLINE, stdin) != NULL)
+    {
+        Rio_writen(clientfd, buf, strlen(buf)); 
+        Rio_readlineb(&rio, buf, MAXLINE);
+        Fputs(buf,stdout);
+    }
+    
     Close(clientfd);
     exit(0);
 
